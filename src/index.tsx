4d7cfe32efd9d74e5bb41751f5abcae181441c7e
明扬 <my@famer.me>
2019-09-06 18:30:58 +0800

    interface

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,33 @@ declare global {
   interface Window { zhaohu: Zhaohu; }
 }
 
+interface InitParam {
+  token: string
+  from: string
+  env?: string
+  basicInfoRequest: () => Promise<any>
+}
+
 class Zhaohu {
-  init (token: string, from: string, env?: string) {
+  init (param: InitParam) {
+    notNull(param.token, 'token')
+    notNull(param.from, 'from')
+    notNull(param.basicInfoRequest, 'basicInfoRequest')
+
     const container = document.createElement('div')
     container.id = "zhaohu"
     document.body.appendChild(container)
 
     ReactDOM.render(<Inject
-      token={token}
-      from={from}
-      env={env}
+      token={param.token}
+      from={param.from}
+      env={param.env}
       />, container);
   }
+}
+
+function notNull (value: any, name?: string) {
+  if (value === null || value === undefined) throw new Error((name || "") + " cannot be null")
 }
 
 
